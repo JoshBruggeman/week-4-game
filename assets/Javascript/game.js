4e4acd093ee82490109fe6f6b9ee33cb7ecba3dd
Josh Bruggeman <joshrbruggeman@gmail.com>
2016-10-28 17:10:36 -0500

    4

--- assets/Javascript/game.js
+++ assets/Javascript/game.js
@@ -1,1 +1,16 @@
-<script>
+var jewelGenerator= function(min,max) {
+	return Math.floor(Math.random()*(max-min+1)+min);
+};
+
+$('#blue').on('click',jewelGenerator(1-12));
+ console.log(jewelGenerator());
+
+$('#red').on('click',jewelGenerator(1-12));
+
+$('#green').on('click',jewelGenerator(1-12));
+
+$('#yellow').on('click,'jewelGenerator(1-12));
+	
+var scoreKeeper= function() {
+	if ()
+}
